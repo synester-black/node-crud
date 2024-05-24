cdcbceaf4cbb62fb86c8c84b86cabb3ec32ef514
synester-black <synester.black@gmail.com>
2024-05-25 03:41:13 +0530

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+app.listen(3000, ()=>{
+    console.log("Running on port 3000");
+});
+
+app.get('/', (req,res) =>{
+    res.send("Hello from node API");
+
+})
