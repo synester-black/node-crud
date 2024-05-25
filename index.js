99a2e1a1c8c2dbfd68716993dc2db0c5ddfc7615
synester-black <synester.black@gmail.com>
2024-05-25 20:26:48 +0530

    db inserted

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express()
+// Using Node.js `require()`
 
 
 app.listen(3000, ()=>{
@@ -9,4 +11,12 @@ app.listen(3000, ()=>{
 app.get('/', (req,res) =>{
     res.send("Hello from node API");
 
+});
+
+mongoose.connect("mongodb+srv://admin:manworkedup123@atlascluster.oogtuou.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
+.then(()=> {
+    console.log("Connected to db");
 })
+.catch(()=>{
+    console.log("Connection failed");
+});
